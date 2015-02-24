4c6e1c9bc0b938b8e6275e92766d693dd9484e26
Domonkos Huszar <husdo@digitus.itk.ppke.hu>
2015-02-24 14:07:07 +0000

    main fix

--- Face Recognition/MainProject/main.cpp
+++ Face Recognition/MainProject/main.cpp
@@ -13,19 +13,21 @@
 
 int main(int argc, char** argv)
 {
-    QApplication app(argc,argv);
+    
 
     if(argc == 1){
+		QApplication app(argc, argv);
         MainWindow mainWindow;
         mainWindow.show();
         return app.exec();
     }
     else{
         Images Imgs(argv[1]);  //test call
+		Images Imgs2(argv[2]);
         EigenFaces first;
         first.training(Imgs);
         //first.load();
-        first.validation(Imgs,GrayImg);
+        first.validation(Imgs2,GrayImg);
         //std::cout<<first.predict(Imgs.getGrayImage(0));
     }
 	return 0;
