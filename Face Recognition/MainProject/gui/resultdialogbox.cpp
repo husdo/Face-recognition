86fab5d7b261a665b2296e0c19d4086c14e7bc21
abeauvisage <axel.beauvisage@gmail.com>
2015-03-06 17:05:59 +0000

    GUI bug fixes Windows

--- Face Recognition/MainProject/gui/resultdialogbox.cpp
+++ Face Recognition/MainProject/gui/resultdialogbox.cpp
@@ -4,7 +4,7 @@
 ResultDialogBox::ResultDialogBox(const Images& img, Facial_Recognizer* recognizer, QWidget* parent) : QDialog(parent), image(0), label(0)
 {
     //definition of the window
-    setWindowFlags(Qt::WindowTitleHint); //title Bar
+    //setWindowFlags(Qt::Window); //title Bar
     setWindowTitle("Recognition"); //title of the window
 
     //setModal(true); //modality
@@ -13,7 +13,7 @@ ResultDialogBox::ResultDialogBox(const Images& img, Facial_Recognizer* recognize
     layout->addWidget(&image,0,Qt::AlignHCenter);
     double c=0;
 	std::string result = recognizer->predict(&c,img.getColorImage(0));
-    label = new QLabel(QString::fromStdString(result.c_str())+" | confidence level: "+QString::number(c));
+    label = new QLabel(QString::fromUtf8(result.c_str())+" | confidence level: "+QString::number(c));
     layout->addWidget(label);
     image.showImage(img.getColorImage(0));
     setLayout(layout); //comment
